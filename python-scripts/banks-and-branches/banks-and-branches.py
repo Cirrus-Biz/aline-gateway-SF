cbf8529aafd8dd345655876415cab8696a12287c
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-04-10 18:19:06 -0400

    CB-42 feature: add file directories for python scripts

---
+++ python-scripts/banks-and-branches/banks-and-branches.py
@@ -0,0 +1,9 @@
+# todo
+# ask how many banks to create
+# post that many random banks to banks
+# ask would you like to add branches to a banks
+# get request for list of banks
+# choose bank from list
+# choose how many branches
+# post that to brances of bank
+
