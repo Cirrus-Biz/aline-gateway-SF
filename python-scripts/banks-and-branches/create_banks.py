49697056f37adeecd36322fd09275956bb117c43
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-04-11 12:31:52 -0400

    CB-42 feature: finished with python logic. Need to write tests

--- python-scripts/banks-and-branches/create_banks.py
+++ python-scripts/banks-and-branches/create_banks.py
@@ -1,37 +1,44 @@
 import requests
 from faker import Faker
+from typing import List
 
 fake = Faker()
 
 BASE = "http://127.0.0.1:8080/api"
 headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdGVwaGVuIiwiYXV0aG9yaXR5IjoiYWRtaW5pc3RyYXRvciIsImlhdCI6MTY0OTY4NTcxNCwiZXhwIjoxNjUwODk1MzE0fQ.Xn8swsXW4P8kEbrJQWDwqGwFQ0SdO6_d1_WM8kmaUxQ"}
 
-def create_banks(number_of_banks: int) -> list[int]:
+# returns list of bank id's created
+def create_banks(number_of_banks: int):
+    try:
+        banks_created_id_array = []
+        
+        banks_created = 0
+        for _ in range(number_of_banks):
 
-    banks_created_id_array = []
-    
-    banks_created = 0
-    for _ in range(number_of_banks):
+            payload = {"routingNumber": fake.aba(),
+                       "address": fake.street_address(),
+                       "city": fake.city(),
+                       "state": fake.state(),
+                       "zipcode": fake.zipcode()}
 
-        payload = {"routingNumber": fake.aba(),
-                   "address": fake.street_address(),
-                   "city": fake.city(),
-                   "state": fake.state(),
-                   "zipcode": fake.zipcode()}
+            response = requests.post(BASE + "/banks", json=payload, headers=headers)
 
-        response = requests.post(BASE + "/banks", json=payload, headers=headers)
+            if response.status_code == 201:
+                banks_created += 1
+                bank_returned_dict = response.json()
+                banks_created_id_array.append(bank_returned_dict["id"])
 
-        if response.status_code == 201:
-            banks_created += 1
-            bank_returned_dict = response.json()
-            banks_created_id_array.append(bank_returned_dict["id"])
+            bank_id = response.json()["id"]
+            bank_city = response.json()["city"]
+            bank_state = response.json()["state"]
+            bank_zipcode = response.json()["zipcode"]
 
-        bank_id = response.json()["id"]
-        bank_city = response.json()["city"]
-        bank_state = response.json()["state"]
-        bank_zipcode = response.json()["zipcode"]
+            print(f"Created Bank: ID:{bank_id} City:{bank_city} State:{bank_state} ZIP:{bank_zipcode}")
 
-        print(f"Created Bank: ID:{bank_id} City:{bank_city} State:{bank_state} ZIP:{bank_zipcode}")
+        print(f"~~~ You Successfully Created {banks_created} Banks ~~~")
+        return banks_created_id_array
 
-    print(f"~~~ You Successfully Created {banks_created} Banks ~~~")
-    return banks_created_id_array
+    except Exception as e:
+        print(e)
+        print("\n(Error!) There Was A Problem Creating Banks...")
+
--- python-scripts/banks-and-branches/create_branches.py
+++ python-scripts/banks-and-branches/create_branches.py
@@ -1,4 +1,3 @@
-# get list of banks
 import requests
 import time
 from faker import Faker
@@ -8,47 +7,9 @@ fake = Faker()
 BASE = "http://127.0.0.1:8080/api"
 headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdGVwaGVuIiwiYXV0aG9yaXR5IjoiYWRtaW5pc3RyYXRvciIsImlhdCI6MTY0OTY4NTcxNCwiZXhwIjoxNjUwODk1MzE0fQ.Xn8swsXW4P8kEbrJQWDwqGwFQ0SdO6_d1_WM8kmaUxQ"}
 
-# def list_banks():
-#
-#     selection = True
-#     while selection:
-#         try:
-#
-#             get_banks = requests.get(BASE + "/banks", headers=headers)
-#             banks_dict = get_banks.json()
-#
-#             banks_list = "\n~~~ Select Bank To Add Branches To By Bank ID ~~~\n"
-#
-#             for i in range(len(banks_dict["content"])):
-#
-#                 bank_info_dict = banks_dict["content"][i]
-#                 bank_id = bank_info_dict["id"]
-#                 bank_address = bank_info_dict["address"]
-#                 bank_city = bank_info_dict["city"]
-#                 bank_state = bank_info_dict["state"]
-#
-#                 banks_list += f"ID:{bank_id} {bank_address}, {bank_city}, {bank_state}\n"
-#
-#             print(banks_list)
-#
-#             bank_id_number = int(input("Select ID: "))
-#
-#             verify_bank = requests.get(BASE + f"/banks/id/{bank_id_number}", headers=headers)
-#             if verify_bank.status_code != 200:
-#                 print("\n(Error!) Please Select Valid ID...")
-#             
-#             else:
-#                 return bank_id_number
-#
-#         except Exception as e:
-#             # logging.error("There Was A Problem Running The Application")
-#             print(e)
-#             print("\n(Error!) There Was A Problem Running The Application...")
-
-
 def create_branches(banks_created_id_array):
+
     try:
-        print(banks_created_id_array)    
         branches_created = 0
         for bank_id in banks_created_id_array:
 
@@ -76,7 +37,6 @@ def create_branches(banks_created_id_array):
 
         print(f"~~~ You Successfully Created {branches_created} Branches For {len(banks_created_id_array)} Banks ~~~")
 
-
     except Exception as e:
         print(e)
-        print("\n(Error!) There Was A Problem Running The Application...")
+        print("\n(Error!) There Was A Problem Creating Branches...")
