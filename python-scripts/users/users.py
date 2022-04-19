e61dc0da7229f68ee6ae4e324387a8474ce8dbcd
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-04-19 19:24:40 -0400

    CB-54 refactor: updated sys paths in users dir to auto generate

--- python-scripts/users/users.py
+++ python-scripts/users/users.py
@@ -1,7 +1,11 @@
+import os
 import sys
-sys.path.append("/Users/stephenfreed/Projects/Capstone/Cirrus-Biz/aline-gateway-SF/python-scripts/users/")
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+sys.path.append(dir_path)
 
 from app import create_admins, create_members
+
 
 selection = True
 while selection:
