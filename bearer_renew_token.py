e614bda8726da9291a05d5615e0f8c6667d3f021
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-04-27 12:28:15 -0400

    CB-54 feature: add bearer_renew_token.py

---
+++ bearer_renew_token.py
@@ -0,0 +1,68 @@
+"""
+    This script attempts to register user.
+    It then logs in with that user to parse header response holding JWT token. 
+    It then writes this token to .env file. 
+    These tokens expire every 7 days.
+    Currently You can run this if getting 403's to refresh token. 
+    In further version we will automate this with springboot scheduling. 
+"""
+
+import schedule
+import time
+from datetime import datetime
+import requests
+
+BASE = "http://127.0.0.1:8080/api"
+
+def renew_bearer():
+    try:
+
+        payload =  {
+                      "username": "stephenfreed",
+                      "password": "Password01!",
+                      "role": "admin",
+                      "firstName": "stephen",
+                      "lastName": "Freed",
+                      "email": "stephen@gmail.com",
+                      "phone": "123-123-1234"}
+               
+
+        # posts data to registration endpoint
+        response = requests.post(BASE + "/users/registration", json=payload)
+        response_code = response.status_code
+
+        with open("./bearer_log.txt", "a") as my_file:
+            todays_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            my_file.write(f"BEARER_TOKEN Registration on {todays_date} ~ Status:{response_code} ")
+
+    except Exception as e:
+        pass
+        # print(e)
+
+    finally:
+
+        payload =  {
+                      "username": "stephenfreed",
+                      "password": "Password01!"}
+
+        # posts data to login
+        response = requests.post(BASE + "/login", json=payload)
+
+        # parses bearer token from headers
+        response_headers = response.headers
+        bearer_token = response_headers["Authorization"][7:]
+
+        # writes to .env file
+        with open("./.env", "w") as my_file:
+            my_file.write(f"BEARER_TOKEN={bearer_token}")
+
+        with open("./bearer_log.txt", "a") as my_file:
+            todays_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            my_file.write(f"BEARER_TOKEN renewed on {todays_date}")
+
+schedule.every(2).minutes.do(renew_bearer)
+# schedule.every().day.at("10:30").do(renew_bearer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
