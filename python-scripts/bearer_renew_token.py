7c36f6369922a5418bc599c0a5d3dd811800dcd8
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-05-11 12:47:20 -0400

    CB-54 refactor: added path dir to bearer_renew_token so the .env and log files it writes will go in same directory every time

--- python-scripts/bearer_renew_token.py
+++ python-scripts/bearer_renew_token.py
@@ -9,15 +9,18 @@
 """
 
 import os
+import sys
 import requests
 from datetime import datetime
 from dotenv import load_dotenv, find_dotenv
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 load_dotenv(find_dotenv())
 ADCU_LOADBALANCER_DNS = os.environ.get("ADCU_LOADBALANCER_DNS")
 
 # HOST = "127.0.0.1"
-HOST = "ENTER REMOTE MACHINE HOST HERE"
+HOST = "ENTER REMOTE HOSTNAME HERE"
 
 BASE = f"http://{HOST}:8080/api"
 
@@ -38,7 +41,7 @@ def renew_bearer():
         response = requests.post(BASE + "/users/registration", json=payload)
         response_code = response.status_code
 
-        with open("./bearer_log.txt", "a") as my_file:
+        with open(f"{dir_path}/bearer_log.txt", "a") as my_file:
             todays_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             my_file.write(f"BEARER_TOKEN Registration on {todays_date} ~ Status:{response_code}\n")
 
@@ -59,10 +62,10 @@ def renew_bearer():
         bearer_token = response_headers["Authorization"][7:]
 
         # writes to .env file
-        with open("./.env", "w") as my_file:
+        with open(f"{dir_path}/.env", "w") as my_file:
             my_file.write(f"BEARER_TOKEN={bearer_token}\nHOST={HOST}")
 
-        with open("./bearer_log.txt", "a") as my_file:
+        with open(f"{dir_path}/bearer_log.txt", "a") as my_file:
             todays_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             my_file.write(f"BEARER_TOKEN renewed on {todays_date}\n")
 
